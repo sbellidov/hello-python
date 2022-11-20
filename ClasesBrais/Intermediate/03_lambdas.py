6e2abd335adb6cf40bb1649bb9cfea3dd9f00263
Sergio Bellido <sbellidov@gmail.com>
2022-11-20 23:10:50 +0100

    Clase lambdas

---
+++ ClasesBrais/Intermediate/03_lambdas.py
@@ -0,0 +1,8 @@
+### Challenges ###
+
+sum_two_values = lambda first_value, second_value: first_value + second_value
+print(sum_two_values(7,8))
+print(sum_two_values(-4,7))
+
+multiply_values = lambda first_value, second_value: first_value * second_value - 3
+print(multiply_values(8,2))
