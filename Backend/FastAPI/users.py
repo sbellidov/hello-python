7dfd750173f5c719f879d234e047e85056e24038
Sergio Bellido <sbellidov@gmail.com>
2023-01-13 23:50:18 +0100

    users

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 # Iniciar el servidor con: uvicorn users:app --reload
 from pydantic import BaseModel
 
@@ -16,14 +16,62 @@ users_list = [User(id=1, name="Sergio",surname="Bellido",age=38),
             User(id=3, name="Alberto",surname="Bellido",age=6)
             ]
 
+# GET ------------------------- Lee
+
 # Devuelve la lista completa de usuarios
-@app.get("/usersclass")
+@app.get("/users")
 async def usersclass():
     return users_list
 
-# Devuelve el usuario según su 'id' indicado en el path
+# ** PATH ** Devuelve el usuario según su 'id' indicado en el path
 @app.get("/userid/{id}")
 async def userid(id: int):
+    return search_user(id)
+
+# ** QUERY ** Consulta de un usuario desde query
+@app.get("/userquery/") # Añadir ==> .../?id=1
+async def user(id: int):
+    return search_user(id)
+
+# POST ------------------------- Inserta
+
+@app.post("/user" , status_code = 201 )
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code = 404 , detail = "El usuario ya existe")
+        # return {"El usuario ya existe"}
+    users_list.append(user)
+    return {"Usuario insertado con éxito !"}
+
+# PUT ------------------------- Modifica
+
+@app.put("/user")
+async def user(user: User):
+    found = False
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error: No se ha actualizado el usuario"}
+    return {"Usuario modificado"}
+
+
+# DELETE ------------------------- Borra
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error: No se ha encontrado el usuario para eliminar"}
+    return {"Usuario borrado"}
+
+
+def search_user(id:int):
     user = filter(lambda user: user.id == id, users_list)
     try:
         return list(user)[0]
@@ -31,7 +79,7 @@ async def userid(id: int):
         return {"error: no se ha encontrado el usuario"}
 
 
-
+#---------------------------------------------
 # Esta forma es desaconsejada, poco eficiente
 @app.get("/usersjson")
 async def usersjson():
@@ -39,3 +87,5 @@ async def usersjson():
             {"name":"Sofia", "surname":"Bellido", "age": 9},
             {"name":"Alberto", "surname":"Bellido", "age": 6}
             ]
+#---------------------------------------------
+
